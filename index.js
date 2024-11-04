27986a7367e7a3c3021a980fe4b868d610f5c48e
qcloud <ubuntu@localhost.localdomain>
2024-11-04 15:11:52 +0800

    test input

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const domains = core.getInput('domains');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`${domains}`);
+  const t = domains.split('\n').map(domain => domain.trim()).filter(Boolean);
+  console.log(`${t}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
