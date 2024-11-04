cbd92140e40c944a19ff879d76ca7dbe922b57d3
qcloud <ubuntu@localhost.localdomain>
2024-11-04 15:13:56 +0800

    test input

--- dist/index.js
+++ dist/index.js
@@ -31817,7 +31817,11 @@ const github = __nccwpck_require__(1685);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const domains = core.getInput('domains');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`${domains}`);
+  const t = domains.split('\n').map(domain => domain.trim()).filter(Boolean);
+  console.log(`${t}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
